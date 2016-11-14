2a8ad7c29f379c56aa579585abddcc1499c2e009
j- <j@skeoh.com>
2016-11-14 15:38:43 +1100

    Add a title to the liability ratio row

--- src/components/intercept-details/rows/liability-ratio.jsx
+++ src/components/intercept-details/rows/liability-ratio.jsx
@@ -3,8 +3,13 @@ import Row from '../intercept-detail-row';
 import Label from '../intercept-detail-label';
 import Value from '../intercept-detail-value';
 
+const title = (
+	'The "expected:actual" liability ratio that scales the amount this bet ' +
+	'will be accepted at where 1 = full stake, 0.5 = half stake etc.'
+);
+
 export default ({ value }) => (
-	<Row>
+	<Row title={ title }>
 		<Label>Liability ratio</Label>
 		<Value value={ value } editable />
 	</Row>
