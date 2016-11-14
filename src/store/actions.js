77e19432fe8a409eac0e9c61ff6653193182407f
j- <j@skeoh.com>
2016-11-14 16:36:49 +1100

    Clamp liability ratio between zero and one

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,9 @@ import {
 	SET_LIABILITY_RATIO,
 } from './types';
 
+const clamp = (val, min, max) => Math.max(min, Math.min(max, val));
+
 export const setLiabilityRatio = (ratio) => ({
 	type: SET_LIABILITY_RATIO,
-	ratio: Number(ratio),
+	ratio: clamp(Number(ratio), 0, 1),
 });
