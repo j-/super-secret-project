a3b86a69924e0775f8ad685d240d1fd68797ab98
j- <j@skeoh.com>
2016-11-14 16:40:36 +1100

    Add dummy reject button to interface

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import InterceptDetails from '../intercept-details';
 import AcceptPartialButton from '../accept-partial-button';
+import RejectButton from '../reject-button';
 
 const App = () => (
 	<div>
 		<AcceptPartialButton interceptId="123" />
+		{ ' ' }
+		<RejectButton />
 		<hr />
 		<InterceptDetails interceptId="123" />
 	</div>
