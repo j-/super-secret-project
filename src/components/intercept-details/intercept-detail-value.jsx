40469f959a3ee11db086ed758f00a72f8229178f
j- <j@skeoh.com>
2016-11-14 11:28:32 +1100

    Style value inputs

--- src/components/intercept-details/intercept-detail-value.jsx
+++ src/components/intercept-details/intercept-detail-value.jsx
@@ -1,28 +1,26 @@
 import React, { PropTypes } from 'react';
-import Editable from './value-editable';
-import Noneditable from './value-noneditable';
 
 import {
 	detailsValue,
+	detailsInputEditable as inputEditable,
+	detailsInputNoneditable as inputNoneditable,
 } from './styles';
 
 const InterceptDetailValue = ({
 	editable,
 	value,
 	...props,
-}) => {
-	let childElement;
-	if (editable) {
-		childElement = <Editable value={ value } { ...props } />;
-	} else {
-		childElement = <Noneditable value={ value } { ...props } />;
-	}
-	return (
-		<div className={ detailsValue }>
-			{ childElement }
-		</div>
-	);
-};
+}) => (
+	<div className={ detailsValue }>
+		<input
+			type="text"
+			defaultValue={ value }
+			readOnly={ !editable }
+			className={ editable ? inputEditable : inputNoneditable }
+			{ ...props }
+		/>
+	</div>
+);
 
 InterceptDetailValue.propTypes = {
 	editable: PropTypes.bool,
